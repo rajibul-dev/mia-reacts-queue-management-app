68bbe36261723cfe4424dba4afb425216d08f23a
Rajibul Islam <rajibulski110@gmail.com>
2023-03-15 02:41:37 +0530

    Done laying out and styling the root page

--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -1,17 +1,22 @@
 import React from "react"
 
+// styles
+import './Root.css'
+
 export default function Root() {
   return (
     <div className="Root">
       <h1>What do you want to do?</h1>
       
-      <button>
-        I’m a viewer and I want to see the queue list
-      </button>
+      <div className="btn-flex-initial">
+        <button>
+          I’m a viewer and I want to see the queue list
+        </button>
 
-      <button>
-        I’m Mia and I want to manage the queue for the stream
-      </button>
+        <button>
+          I’m Mia and I want to manage the queue for the stream
+        </button>
+      </div>
     </div>
   )
 }
