539727553967cbd97d5134365d4cb255160a5d27
Rajibul Islam <rajibulski110@gmail.com>
2023-03-17 04:25:11 +0530

    view page design done

---
+++ src/components/QueueListView.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+// styles
+import './QueueListView.css'
+
+export default function QueueListView({ queues }) {
+  return (
+    <ol className="queue-list-in-both QueueListView">
+      {queues.map((queue) => (
+        <li key={queue.id}>
+          <h2>{queue.onNumber}</h2>
+          <div className="flex-name-n-btn">
+          <p>{queue.name}</p>
+          <a href={queue.videoLink} target='_blank'>
+          </a>
+          </div>
+        </li>
+      ))}
+    </ol>
+  )
+}
--- src/pages/viewer/Viewer.jsx
+++ src/pages/viewer/Viewer.jsx
@@ -1,14 +1,33 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import { useCollection } from "../../hooks/useCollection";
+
+// components
+import QueueListView from "../../components/QueueListView";
 
 // styles
-import './Viewer.css'
+import "./Viewer.css";
 
 export default function Viewer() {
+  const { documents, error } = useCollection("queueList");
+  const [queueList, setQueueList] = useState([]);
+
+  useEffect(() => {
+    if (documents) {
+      // Sort the queue list based on the onNumber property
+      const sortedList = documents.sort(
+        (a, b) => a.onNumber - b.onNumber
+      );
+      setQueueList(sortedList);
+    }
+  }, [documents]);
+
   return (
     <div className="Viewer">
       <div className="sm-container">
         <h1>Mia Reacts Queue List</h1>
+        {error && <p>{error}</p>}
+        {queueList && <QueueListView queues={queueList} />}
       </div>
     </div>
-  )
+  );
 }
