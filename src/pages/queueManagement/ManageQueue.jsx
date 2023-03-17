68aed8b42233fdace9495bbb59b282abeff56a74
Rajibul Islam <rajibulski110@gmail.com>
2023-03-18 03:00:31 +0530

    successfully implemented auth without auth

--- src/pages/queueManagement/ManageQueue.jsx
+++ src/pages/queueManagement/ManageQueue.jsx
@@ -1,9 +1,14 @@
 import React from "react"
 
+// styles
+import './ManageQueue.css'
+const isAdmin = localStorage.getItem("isAdmin") === "true";
+
 export default function ManageQueue() {
   return (
     <div>
-      
+      {isAdmin && <h1>admin</h1>}
+      {!isAdmin && <h1>not admin</h1>}
     </div>
   )
 }
