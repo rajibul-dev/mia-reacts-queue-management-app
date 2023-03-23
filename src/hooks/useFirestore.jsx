fec4bec5e2ab4048b749112d6da4acae786dd60b
Rajibul Islam <rajibulski110@gmail.com>
2023-03-23 19:11:18 +0530

    create, update, delete functions of firestore are now implemented with a new hook called useFirestore

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -1,78 +1,38 @@
-import { useReducer, useEffect, useState } from "react";
-import { db } from "../firebase/config";
-import { collection, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
-
-let initialState = {
-  document: null,
-  isPending: false,
-  error: null,
-  success: null
-}
-
-const firestoreReducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_DOCUMENT_PENDING":
-    case "UPDATE_DOCUMENT_PENDING":
-    case "DELETE_DOCUMENT_PENDING":
-      return { ...state, isPending: true, success: null, error: null }
-    case "ADD_DOCUMENT_SUCCESS":
-    case "UPDATE_DOCUMENT_SUCCESS":
-    case "DELETE_DOCUMENT_SUCCESS":
-      return { ...state, isPending: false, success: action.payload, error: null }
-    case "ADD_DOCUMENT_ERROR":
-    case "UPDATE_DOCUMENT_ERROR":
-    case "DELETE_DOCUMENT_ERROR":
-      return { ...state, isPending: false, success: null, error: action.payload }
-    default:
-      return state
-  }
-}
+import { useEffect, useState } from "react"
+import { db } from "../firebase/config"
+import { collection, addDoc, doc, setDoc, deleteDoc, onSnapshot } from "firebase/firestore";
 
 export const useFirestore = (col) => {
-  const [response, dispatch] = useReducer(firestoreReducer, initialState)
-  const [isCancelled, setIsCancelled] = useState(false)
-
-  // set reference
-  let collectionStr = col
-  const ref = collection(db, collectionStr)
+  const [error, setError] = useState(null)
 
   const addDocument = async (data) => {
-    dispatch({ type: "ADD_DOCUMENT_PENDING" })
     try {
-      const docRef = await addDoc(ref, data)
-      dispatch({ type: "ADD_DOCUMENT_SUCCESS", payload: docRef })
-    } catch (error) {
-      dispatch({ type: "ADD_DOCUMENT_ERROR", payload: error.message })
+      await addDoc(collection(db, col), data)
+    } catch (err) {
+      console.error(err)
+      setError('Could not add the document')
     }
   }
 
   const updateDocument = async (id, data) => {
-    dispatch({ type: "UPDATE_DOCUMENT_PENDING" })
     try {
-      const docRef = doc(ref, id)
-      await updateDoc(docRef, data)
-      dispatch({ type: "UPDATE_DOCUMENT_SUCCESS", payload: id })
-    } catch (error) {
-      dispatch({ type: "UPDATE_DOCUMENT_ERROR", payload: error.message })
+      const docRef = doc(db, col, id)
+      await setDoc(docRef, data, { merge: true })
+    } catch (err) {
+      console.error(err)
+      setError('Could not update the document')
     }
   }
 
   const deleteDocument = async (id) => {
-    dispatch({ type: "DELETE_DOCUMENT_PENDING" })
     try {
-      const docRef = doc(ref, id)
+      const docRef = doc(db, col, id)
       await deleteDoc(docRef)
-      dispatch({ type: "DELETE_DOCUMENT_SUCCESS", payload: id })
-    } catch (error) {
-      dispatch({ type: "DELETE_DOCUMENT_ERROR", payload: error.message })
+    } catch (err) {
+      console.error(err)
+      setError('Could not delete the document')
     }
   }
 
-  useEffect(() => {
-    return () => {
-      setIsCancelled(true)
-    }
-  }, [])
-
-  return { response, addDocument, updateDocument, deleteDocument }
+  return { error, addDocument, updateDocument, deleteDocument }
 }
