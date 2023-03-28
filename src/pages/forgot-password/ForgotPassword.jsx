98aa66744febb626bf3c8120ebbe43f6444b8390
Rajibul Islam <rajibulski110@gmail.com>
2023-03-28 20:51:45 +0530

    reset password and forgot password page markup done and style done

--- src/pages/forgot-password/ForgotPassword.jsx
+++ src/pages/forgot-password/ForgotPassword.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+
+// styles
+import './ForgotPassword.css'
 
 export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email);
+  }
+
   return (
-    <div>
-      Forgot Password
+    <div className="ForgotPassword">
+      <div className="fp-container">
+        <h1>Forgot password</h1>
+        <p className="p">
+          A password-reset link will be sent to you in your email
+        </p>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="not-password-input"
+            type='email'
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            autoFocus
+          />
+
+          {errorMessage && <p className="error">{errorMessage}</p>}
+
+          <div className="two-btn">
+            <button 
+              className="submit-btn"
+              type="submit"
+            >
+              Get password recovery link
+            </button>
+          </div>
+
+          <p className="p-under-btn">Go back to login page <Link to='/login' className="forgot-password a-under-btn">Login</Link></p>
+        </form>
+      </div>
     </div>
   )
 }
--- src/pages/reset-password/ResetPassword.jsx
+++ src/pages/reset-password/ResetPassword.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react"
+
+// styles
+import './ResetPassword.css'
+import VisibilityOnIcon from '../../icons/visibility.svg';
+import VisibilityOffIcon from '../../icons/visibility-off.svg';
 
 export default function ResetPassword() {
+  const [password, setPassword] = useState("");
+  const [confPass, setConfPass] = useState("")
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(password, confPass);
+  }
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
   return (
-    <div>
-      Reset Password
+    <div className="ResetPassword">
+      <div className="rp-container">
+        <h1>Reset password</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="New password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <img 
+              className="eye-btn"
+              src={showPassword ? VisibilityOnIcon : VisibilityOffIcon}
+              alt={showPassword ? "visibility on" : "visibility off"}
+              onClick={toggleShowPassword}
+            />
+          </div>
+          <input
+            className="not-password-input conform-password"
+            type='password'
+            placeholder="Conform new password"
+            value={confPass}
+            onChange={(e) => setConfPass(e.target.value)}
+            required
+          />
+
+          {errorMessage && <p className="error">{errorMessage}</p>}
+
+          <div className="two-btn">
+            <button 
+              className="submit-btn"
+              type="submit"
+            >
+              Reset password
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
