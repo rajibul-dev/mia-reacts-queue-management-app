2aaced58da56eaca3ccda41ddb30a3fadb42722a
Rajibul Islam <rajibulski110@gmail.com>
2023-03-29 20:08:29 +0530

    useLogout hook base done

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,41 +1,18 @@
-import { useEffect, useState } from 'react'
-import { projectAuth } from '../firebase/config'
-import { useAuthContext } from './useAuthContext'
+import { auth } from '../firebase/config'
+import { signOut } from 'firebase/auth'
 
 export const useLogout = () => {
-  const [isCancelled, setIsCancelled] = useState(false)
-  const [error, setError] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
-  
-  const logout = async () => {
-    setError(null)
-    setIsPending(true)
 
-    try {
-      // sign the user out
-      await projectAuth.signOut()
-      
-      // dispatch logout action
-      dispatch({ type: 'LOGOUT' })
-
-      // update state
-      if (!isCancelled) {
-        setIsPending(false)
-        setError(null)
-      } 
-    } 
-    catch(err) {
-      if (!isCancelled) {
-        setError(err.message)
-        setIsPending(false)
-      }
-    }
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('User signed out');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      })
   }
 
-  useEffect(() => {
-    return () => setIsCancelled(true)
-  }, [])
+  return {logout}
 
-  return { logout, error, isPending }
 }
