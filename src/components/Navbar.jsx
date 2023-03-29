54612f9e425bea9c526bdde644713890c858ebbd
Rajibul Islam <rajibulski110@gmail.com>
2023-03-29 21:53:55 +0530

    logout button and welcome message created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link, useLocation } from "react-router-dom"
+import { useAuthContext } from "../hooks/useAuthContext"
+import { useLogout } from '../hooks/useLogout'
 
 // styles
 import './Navbar.css'
@@ -9,21 +11,46 @@ export default function Navbar() {
   const location = useLocation();
   const { pathname } = location;
 
+  const { user } = useAuthContext()
+  const { logout } = useLogout()
+
   return (
     <header className="Navbar">
       <div className="sm-container nav-container">
-        <Link to='/'>
-          <button>
-            <img
-              className="back-icon-svg"
-              src={BackBtn}
-              alt="left arrow / back button"
-            />
-            <span></span>
-          </button>
-        </Link>
+        {pathname !== '/' ?
+          <Link to='/'>
+            <button>
+              <img
+                className="back-icon-svg"
+                src={BackBtn}
+                alt="left arrow / back button"
+              />
+              <span></span>
+            </button>
+          </Link>
+          :
+          <span className="welcome-message">
+            {user && `Welcome, ${user.displayName}`}
+          </span>
+        }
 
         <ul>
+          {pathname === '/' && (
+              <>
+                <li>
+                  {user ?
+                  <Link onClick={logout}>
+                    Log out
+                  </Link> 
+                  :
+                  <Link to='/login'>
+                    Login
+                  </Link>
+                  }
+                </li>
+              </>
+            )
+          }
           {pathname === '/viewer' && (
               <>
                 <li>
--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -1,29 +1,34 @@
 import React from "react"
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 
 // styles
 import './Root.css'
 
 export default function Root() {
   return (
-    <div className="Root">
-      <h1>What do you want to do?</h1>
+    <>
+      <Navbar />
       
-      <div className="btn-flex-initial">
+      <div className="Root">
+        <h1>What do you want to do?</h1>
+        
+        <div className="btn-flex-initial">
 
-        <Link to='/viewer'>
-          <button>
-            I’m a user and I want to see the queue list
-          </button>
-        </Link>
+          <Link to='/viewer'>
+            <button>
+              I’m a user and I want to see the queue list
+            </button>
+          </Link>
 
-        <Link to='/verify' state={{ route: '/manage-queue', cancelRoute: '/' }}>
-          <button>
-            I’m Mia and I want to manage the queue list
-          </button>
-        </Link>
+          <Link to='/verify' state={{ route: '/manage-queue', cancelRoute: '/' }}>
+            <button>
+              I’m Mia and I want to manage the queue list
+            </button>
+          </Link>
 
+        </div>
       </div>
-    </div>
+    </>
   )
 }
