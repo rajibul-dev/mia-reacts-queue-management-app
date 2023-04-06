18e5f6a603605ce14b94943ce18cd5417663a8bc
Rajibul Islam <rajibulski110@gmail.com>
2023-04-07 03:25:10 +0530

    changed the button text for /viewer Link btn

--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -1,34 +1,33 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 
 // styles
-import './Root.css'
+import "./Root.css";
 
 export default function Root() {
   return (
     <>
       <Navbar />
-      
+
       <div className="Root">
         <h1>What do you want to do?</h1>
-        
+
         <div className="btn-flex-initial">
-
-          <Link to='/viewer'>
+          <Link to="/viewer">
             <button>
-              I’m a user and I want to see the queue list
+              I’m a user and I want to join or view the queue list
             </button>
           </Link>
 
-          <Link to='/verify' state={{ route: '/manage-queue', cancelRoute: '/' }}>
-            <button>
-              I’m Mia and I want to manage the queue list
-            </button>
+          <Link
+            to="/verify"
+            state={{ route: "/manage-queue", cancelRoute: "/" }}
+          >
+            <button>I’m Mia and I want to manage the queue list</button>
           </Link>
-
         </div>
       </div>
     </>
-  )
+  );
 }
