0c71c67c85e9f8361e7e347b66852d2ef024fcf8
Rajibul Islam <rajibulski110@gmail.com>
2023-04-13 17:52:14 +0530

    toast for addManually section

--- src/components/queue-related/QAddManually.jsx
+++ src/components/queue-related/QAddManually.jsx
@@ -4,11 +4,21 @@ import { useFirestore } from "../../hooks/useFirestore";
 // styles
 import "./QAddManually.css";
 
+// components
+import CustomToast from "../CustomToast";
+
 export default function QAddManually({ queues }) {
   const [videoLink, setVideoLink] = useState("");
   const [user, setUser] = useState("");
+  const [manualErr, setManualError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
   const { addDocument, error, isPending } = useFirestore("queueList");
+
+  const handleToastClose = () => {
+    setManualError(null);
+    setSuccess(null);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,37 +30,57 @@ export default function QAddManually({ queues }) {
     });
     setVideoLink("");
     setUser("");
+    setSuccess(`Successfully added ${user} to the list!`);
   };
 
   return (
-    <section
-      className={`QAddManually${queues.length === 0 ? " more-margin" : ""}`}
-    >
-      <h2>Add to queue list manually</h2>
-      <form onSubmit={handleSubmit} className="add-manually-form">
-        <input
-          className="user"
-          type="text"
-          placeholder="User Name"
-          onChange={(e) => setUser(e.target.value)}
-          value={user}
-          required
-        />
+    <>
+      <section
+        className={`QAddManually${queues.length === 0 ? " more-margin" : ""}`}
+      >
+        <h2>Add to queue list manually</h2>
+        <form onSubmit={handleSubmit} className="add-manually-form">
+          <input
+            className="user"
+            type="text"
+            placeholder="User Name"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+          />
 
-        <input
-          className="video-link"
-          type="url"
-          placeholder="Paste the video link here"
-          onChange={(e) => setVideoLink(e.target.value)}
-          value={videoLink}
-          required
-        />
+          <input
+            className="video-link"
+            type="url"
+            placeholder="Paste the video link here"
+            onChange={(e) => setVideoLink(e.target.value)}
+            value={videoLink}
+            required
+          />
 
-        <button className="flex-grow" type="submit" disabled={isPending}>
-          Add to list
-        </button>
-        {error && <p className="error">{error}</p>}
-      </form>
-    </section>
+          <button className="flex-grow" type="submit" disabled={isPending}>
+            Add to list
+          </button>
+        </form>
+      </section>
+      {error && (
+        <CustomToast
+          message={error}
+          type="error"
+          duration={5000}
+          onClose={handleToastClose}
+          margin="err-margin-3"
+        />
+      )}
+      {success && (
+        <CustomToast
+          message={success}
+          type="success"
+          duration={5000}
+          onClose={handleToastClose}
+          margin="err-margin-3"
+        />
+      )}
+    </>
   );
 }
